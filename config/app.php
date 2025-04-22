aaabedf66bd7bcbe370a027c42a5a3583f0efd37
JeanDev-10 <jeanpierrerodriguezzambrano@gmail.com>
2025-04-22 14:40:34 -0500

    fix: :fire: revert url email verification with frontend

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
     'frontend_url' => env('APP_FRONTEND_URL', 'http://localhost:4200'),
-    'frontend_url_path_email_confirmation' => env('APP_FRONTEND_URL_PATH_EMAIL_VERIFICATION', '/menu/email/verify?'),
+    'frontend_url_path_email_confirmation' => env('APP_FRONTEND_URL_PATH_EMAIL_VERIFICATION', '/email/verify?'),
 
     /*
     |--------------------------------------------------------------------------
