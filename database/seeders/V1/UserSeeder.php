635f09246043036589a98cc231b363f970e02398
Jahir Celorio <jahir.celorio2001@gmail.com>
2025-09-24 11:32:49 -0500

    feat: :sparkles: seeders resolved bug

--- database/seeders/V1/UserSeeder.php
+++ database/seeders/V1/UserSeeder.php
@@ -11,26 +11,42 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-        // Usuario admin
-        $admin = User::create([
-            'name' => "Frowen",
-            'lastname' => 'Sacón',
-            'username' => 'FrowenS10',
-            'email' => "agrotechconnect2025@gmail.com",
-            'password' => bcrypt("R7pwA3vLz!9eQ"),
-            'registration_method' => 'local',
-            'email_verified_at' => now(),
-        ]);
+        // Crear roles primero
+        $admin_role = Role::firstOrCreate(["name" => "admin"]);
+        $client_role = Role::firstOrCreate(["name" => "client"]);
+        $client_role_social = Role::firstOrCreate(["name" => "client_social"]);
 
-        // Crear rol admin y permisos básicos que necesita
-        $admin_role = Role::create(["name" => "admin"]);
-
-        // Crear los permisos que el admin necesita
+        // Crear permisos
         Permission::firstOrCreate(["name" => "user.change-password"]);
         Permission::firstOrCreate(["name" => "user.upload-avatar"]);
         Permission::firstOrCreate(["name" => "admin.delete-account"]);
+        Permission::firstOrCreate(["name" => "user.delete-account"]);
+        Permission::firstOrCreate(["name" => "user.delete-account-social"]);
+        Permission::firstOrCreate(["name" => "post.create-complaint"]);
+        Permission::firstOrCreate(["name" => "comment.create-complaint"]);
+        Permission::firstOrCreate(["name" => "replyComment.create-complaint"]);
 
-        $admin->assignRole("admin");
+        // Usuario admin
+        $admin = User::firstOrCreate(
+            ['email' => "agrotechconnect2025@gmail.com"],
+            [
+                'name' => "Frowen",
+                'lastname' => 'Sacón',
+                'username' => 'FrowenS10',
+                'password' => bcrypt("R7pwA3vLz!9eQ"),
+                'registration_method' => 'local',
+                'email_verified_at' => now(),
+            ]
+        );
+
+        // Asignar permisos a roles
         $admin_role->syncPermissions(['user.change-password', 'user.upload-avatar', 'admin.delete-account']);
+        $client_role->syncPermissions(['user.change-password', 'user.upload-avatar', 'post.create-complaint', 'comment.create-complaint', 'replyComment.create-complaint', 'user.delete-account']);
+        $client_role_social->syncPermissions(['user.delete-account-social', 'post.create-complaint', 'comment.create-complaint', 'replyComment.create-complaint']);
+
+        // Asignar rol al admin
+        if (!$admin->hasRole('admin')) {
+            $admin->assignRole("admin");
+        }
     }
 }
