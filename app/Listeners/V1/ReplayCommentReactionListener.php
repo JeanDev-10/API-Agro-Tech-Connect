9e4539623ee4d7518fba92deca4652c1181ea086
JeanDev-10 <jeanpierrerodriguezzambrano@gmail.com>
2025-09-27 00:33:38 -0500

    feat: :fire: don´t notify myself

--- app/Listeners/V1/ReplayCommentReactionListener.php
+++ app/Listeners/V1/ReplayCommentReactionListener.php
@@ -4,14 +4,19 @@ namespace App\Listeners\V1;
 
 use App\Events\V1\ReplayCommentReactionEvent;
 use App\Notifications\V1\NewReactionReplayCommentNotification;
+use App\Traits\V1\SkipsSelfNotification;
 
 class ReplayCommentReactionListener
 {
+    use SkipsSelfNotification; //trait para evitar notificaciones a uno mismo
     /**
      * Handle the event.
      */
     public function handle(ReplayCommentReactionEvent $event): void
     {
+        if (!$this->shouldNotify($event->reaction->user, $event->replayComment->user)) {
+            return; // No enviar notificación si el usuario es el mismo
+        }
         // Notificar al dueño del comentario sobre la nueva reacción
         $event->replayComment->user->notify(
             new NewReactionReplayCommentNotification($event->replayComment, $event->reaction)
